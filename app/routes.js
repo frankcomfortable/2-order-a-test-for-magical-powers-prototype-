4c54543e0f6ca4e30167732df2faaef1a5105103
Frank Comfortable <fredconstable@gmail.com>
2025-09-09 14:48:57 +0000

    Added a hilarious ineligiblity page

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,21 @@ const express = require('express')
 
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+router.post('/magical-powers-answer', (req, res) => {
+
+    const sobrietyLevel = req.session.data.sobrietyLevel
+    
+   if(sobrietyLevel == "Sober"){
+    res.redirect('/details')
+   }
+   else if (sobrietyLevel == "High as a kite" || sobrietyLevel == "Absolutely baked"){
+    res.redirect('/ineligible')
+   }
+   else {
+    res.redirect('/magical-powers')
+   }
+});
+
+
 
 module.exports = router
